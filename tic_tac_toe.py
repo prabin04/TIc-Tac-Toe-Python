363853279015349945ff830e2941ab12c4d28bf7
Prabin Parajuli <prabin.b9@gmail.com>
2020-05-14 10:06:58 +0200

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,18 @@
 #Creating Tic Tac Toe Game where we ask two users for input and user can put their cross or cirle  wherever they want.
+
+# / / 
+#-----
+# / / 
+#-----
+# / / 
+def drawField():
+    for row in range(5):
+        if row%2 == 0:
+            #print writing lines
+            for column in range(5):
+                if column%2 == 0:
+                    print(" ")
+        else:
+            print("-----")
+    return drawField()
+
